45f7b96045d4fb6ed71a1788ccc26eec4e80c99d
korczoczek <danielkorczok@gmail.com>
2025-11-02 17:16:40 +0100

    yarr harr

---
+++ Content.Server/_Carpmosia/Pirates/PirateComponents.cs
@@ -0,0 +1,11 @@
+namespace Content.Server.Pirates.Components;
+
+/// <summary>
+///     A component added to entities not counted during the pirate plunder appraisal.
+///     Reserved for all the equipment present in the pirate shuttle.
+/// </summary>
+[RegisterComponent]
+public sealed partial class PirateEquipmentComponent : Component
+{
+
+}
---
+++ Content.Server/_Carpmosia/Pirates/PirateSystem.cs
@@ -0,0 +1,29 @@
+using Content.Server.Cargo.Systems;
+using Content.Server.Pirates.Components;
+
+namespace Content.Server.Pirates;
+
+public sealed partial class PirateSystem : EntitySystem
+{
+    [Dependency] private readonly ILogManager _logManager = default!;
+
+    private ISawmill _sawmill = default!;
+
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        _sawmill = _logManager.GetSawmill("Pirates");
+    }
+
+    private void MarkPirateEquipment(EntityUid grid)
+    {
+        var xform = Transform(grid);
+        var enumerator = xform.ChildEnumerator;
+        var pirateEquipment = new PirateEquipmentComponent{};
+        while (enumerator.MoveNext(out var child))
+        {
+            EntityManager.AddComponent(child, pirateEquipment, true);
+        }
+    }
+}
